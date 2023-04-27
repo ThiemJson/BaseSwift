ffb23fdf4b21c737f7f0414edb5edaa5c4d6a5a5
thiemnc <thiemnc@vnpt-technology.vn>
2023-04-27 16:20:22 +0700

    Fix pod file

--- base-swift/Pods/Target Support Files/Pods-base-swift-stag/Pods-base-swift-stag-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_base_swift_stag : NSObject
-@end
-@implementation PodsDummy_Pods_base_swift_stag
-@end
--- base-swift/Pods/Target Support Files/Pods-base-swiftTests/Pods-base-swiftTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_base_swiftTests : NSObject
-@end
-@implementation PodsDummy_Pods_base_swiftTests
-@end
